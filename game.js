ab80deeb731a8d4dfa6be55474f19460f75653d4
Javeria shaikh <158833259+javeriashaikh24@users.noreply.github.com>
2024-06-27 07:52:24 +0500

    Add files via upload

---
+++ game.js
@@ -0,0 +1,147 @@
+//  var userInp = prompt("find the words");
+//  var search ;
+//  if(userInp ){
+//     document.write(<div class="card" style="width: 18rem;">
+//         <div class="card-body">
+//           <h5 class="card-title">Card title</h5>
+//           <h6 class="card-subtitle mb-2 text-body-secondary">Card subtitle</h6>
+//           <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+//           <a href="#" class="card-link">Card link</a>
+//           <a href="#" class="card-link">Another link</a>
+//         </div>
+//       </div>)
+//  }
+
+var word = prompt("Enter your search word");
+
+if (temprature <= 39 && temprature >= 33) {
+  document.write('<div  class="card w-50 m-auto"   " style="width: 18rem;">' + '<table border="8"/>;' +
+    '<tr>' +
+       ' <td>' +a+ '</td>'+
+       ' <td>'+d+'</td>'+
+       ' <td>'+j+'</td>'+
+       ' <td>'+f+'</td>'+
+       ' <td>'+g+'</td>'+
+       ' <td>'+t+'</td>'+
+       ' <td>'+a+'</td>'+
+       ' <td>'+b+'</td>'+
+       ' <td>'+z+'</td>'+
+       ' <td>'+k+'</td>'+
+    '</tr>'+
+    '<tr>'+
+       ' <td>'+q+'</td>'+
+       ' <td>'+r+'</td>'+
+       ' <td>'+a+'</td>'+
+       ' <td>'+p+'</td>'+
+       ' <td>'+e+'</td>'+
+       ' <td>'+f+'</td>'+
+       ' <td>'+s+'</td>'+
+       ' <td>'+o+'</td>'+
+       ' <td>'+y+'</td>'+
+       ' <td>'+u+'</td>'+
+    '</tr>'+
+    '<tr>'+
+       ' <td>'+s+'</td>'+
+       ' <td>'+o+'</td>'+
+       ' <td>'+v+'</td>'+
+       ' <td>'+j+'</td>'+
+       ' <td>'+l+'</td>'+
+       ' <td>'+o+'</td>'+
+       ' <td>'+e+'</td>'+
+       ' <td>'+c+'</td>'+
+       ' <td>'+s+'</td>'+
+       ' <td>'+s+'</td>'+
+    '</tr>'+
+    '<tr>'+
+        '<td>'+l+'</td>'+
+        '<td>'+r+'</td>'+
+        '<td>'+a+'</td>'+
+        '<td>'+s+'</td>'+
+        '<td>'+t+'</td>'+
+        '<td>'+m+'</td>'+
+        '<td>'+n+'</td>'+
+        '<td>'+c+'</td>'+
+        '<td>'+z+'</td>'+
+        '<td>'+o+'</td>'+
+    '</tr>'+
+    '<tr>'+
+        '<td>'+p+'</td>'+
+        '<td>'+v+'</td>'+
+        '<td>'+s+'</td>'+
+        '<td>'+n+'</td>'+
+        '<td>'+x+'</td>'+
+        '<td>'+o+'</td>'+
+        '<td>'+o+'</td>'+
+        '<td>'+g+'</td>'+
+        '<td>'+m+'</td>'+
+        '<td>'+a+'</td>'+
+    '</tr>'+
+    '<tr>'+
+        '<td>'+e+'</td>'+
+        '<td>'+v+'</td>'+
+        '<td>'+c+'</td>'+
+        '<td>'+g+'</td>'+
+        '<td>'+d+'</td>'+
+        '<td>'+b+'</td>'+
+        '<td>'+c+'</td>'+
+        '<td>'+o+'</td>'+
+        '<td>'+b+'</td>'+
+        '<td>'+x+'</td>'+
+    '</tr>'+
+    '<tr>'+
+        '<td>'+w+'</td>'+
+        '<td>'+o+'</td>'+
+        '<td>'+r+'</td>'+
+        '<td>'+p+'</td>'+
+        '<td>'+y+'</td>'+
+        '<td>'+t+'</td>'+
+        '<td>'+h+'</td>'+
+        '<td>'+o+'</td>'+
+        '<td>'+n+'</td>'+
+        '<td>'+o+'</td>'+
+    '</tr>'+
+    '<tr>'+
+        '<td>'+t+'</td>'+
+        '<td>'+b+'</td>'+
+        '<td>'+i+'</td>'+
+        '<td>'+s+'</td>'+
+        '<td>'+d+'</td>'+
+        '<td>'+b+'</td>'+
+        '<td>'+i+'</td>'+
+        '<td>'+m+'</td>'+
+        '<td>'+k+'</td>'+
+        '<td>'+i+'</td>'+
+    '</tr>'+
+    '<tr>'+
+        '<td>'+y+'</td>'+
+        '<td>'+o+'</td>'+
+        '<td>'+p+'</td>'+
+        '<td>'+b+'</td>'+
+        '<td>'+x+'</td>'+
+        '<td>'+h+'</td>'+
+        '<td>'+t+'</td>'+
+        '<td>'+m+'</td>'+
+        '<td>'+l+'</td>'+
+        '<td>'+b+'</td>'+
+    '</tr>'+
+    '<tr>'+
+        '<td>'+l+'</td>'+
+        '<td>'+c+'</td>'+
+        '<td>'+t+'</td>'+
+        '<td>'+b+'</td>'+
+        '<td>'+o+'</td>'+
+        '<td>'+d+'</td>'+
+        '<td>'+x+'</td>'+
+        '<td>'+p+'</td>'+
+        '<td>'+e+'</td>'+
+        '<td>'+a+'</td>'+
+   ' </tr>'+
+'</table>'+
+'<h3>' +'javascript'+ '<br>' + 'css' + '<br>'+ 'html' +'<br>' +'python'+ '<br>' +'</h3>'+
+
+'</div>'+
+'</div>'
+  
+    
+)
+}
